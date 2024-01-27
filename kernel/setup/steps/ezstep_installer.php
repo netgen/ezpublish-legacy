d4201c89e7c96a801973c28211b3084485cd14b3
7x <info@se7enx.com>
2024-01-27 04:23:27 +0000

    Updated: Refactor database type text name

--- kernel/setup/steps/ezstep_installer.php
+++ kernel/setup/steps/ezstep_installer.php
@@ -383,7 +383,7 @@ class eZStepInstaller
         $dbParameters = array_merge( $dbParameters, $overrideDBParameters );
 
         // SQLite requires us to specifiy a database name
-        if( $dbParameters['database'] == '' and $this->PersistenceList['database_info']['type'] == 'sqlite' )
+        if( $dbParameters['database'] == '' and $this->PersistenceList['database_info']['type'] == 'sqlite3' )
             $dbParameters['database'] = 'sqlite.db';
 
         // PostgreSQL requires us to specify database name.
