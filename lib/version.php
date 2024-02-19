05d6b7e539365ffc01f149fa186fc88b78c55d68
7x <info@se7enx.com>
2024-02-19 05:51:11 -0800

    Update version.php retain existing repository version information

--- lib/version.php
+++ lib/version.php
@@ -14,13 +14,13 @@
 
 class eZPublishSDK
 {
-    const VERSION_MAJOR = 6;
-    const VERSION_MINOR = 0;
-    const VERSION_RELEASE = 2;
-    const VERSION_STATE = 'stable';
-    const VERSION_DEVELOPMENT = false;
-    const VERSION_ALIAS = '6.0';
-    const EDITION = 'eZ Publish';
+    const VERSION_MAJOR = 5;
+    const VERSION_MINOR = 99;
+    const VERSION_RELEASE = 99;
+    const VERSION_STATE = 'alpha1';
+    const VERSION_DEVELOPMENT = true;
+    const VERSION_ALIAS = '5.99.99';
+    const EDITION = 'eZ Publish Legacy';
 
     /*!
       \return the SDK version as a string
